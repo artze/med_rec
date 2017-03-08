2a8e44597a66bcbd9e19adbb38b4e90d9a91889c
artze <arthurwtm@gmail.com>
2017-03-08 16:00:29 +0800

    changed user-email column to not-null

---
+++ db/migrate/20170308075845_change_email_null_in_users.rb
@@ -0,0 +1,5 @@
+class ChangeEmailNullInUsers < ActiveRecord::Migration[5.0]
+  def change
+  	change_column_null :users, :email, false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170308074100) do
+ActiveRecord::Schema.define(version: 20170308075845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20170308074100) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
-    t.string   "email"
+    t.string   "email",                          null: false
     t.string   "encrypted_password", limit: 128, null: false
     t.string   "confirmation_token", limit: 128
     t.string   "remember_token",     limit: 128, null: false
