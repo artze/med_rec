440e9f78b5ee189aca33e15eeb0cd7a82762c950
artze <arthurwtm@gmail.com>
2017-03-08 15:53:24 +0800

    migrated users table

--- db/migrate/20170308074100_create_users.rb
+++ db/migrate/20170308074100_create_users.rb
@@ -6,6 +6,14 @@ class CreateUsers < ActiveRecord::Migration[5.0]
       t.string :encrypted_password, limit: 128, null: false
       t.string :confirmation_token, limit: 128
       t.string :remember_token, limit: 128, null: false
+      t.string :full_name
+      t.string :identity_card
+      t.string :street_address
+      t.string :postcode
+      t.string :city
+      t.string :country
+      t.string :contact_number
+      t.string :email
     end
 
     add_index :users, :email
