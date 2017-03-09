b20fc8f487213f06e5609f43f1271053fb3ac138
artze <arthurwtm@gmail.com>
2017-03-09 15:30:41 +0800

    created and populated medical_conditions table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+CSV.foreach('../scrape_disease_names/diseases.csv') do |row|
+	MedicalCondition.create(name: row[0])
+end
