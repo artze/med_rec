e4cb3a4cfe79803ba528afb9eb893028344d8bf7
6akayas <sayakach6@gmail.com>
2017-03-09 14:52:07 +0800

    fixed sign up error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ end
 def create
   @user = User.new(user_params)
   respond_to do |format|
-    if @user.save
+    if @user.save!
       format.html { redirect_to @user, notice: 'User was successfully created.' }
       # format.js   {}
       format.json { render json: @user, status: :created, location: @user }
