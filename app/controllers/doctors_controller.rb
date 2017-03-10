85e78ba53ae2a881dec5c23610e00639137d24ae
artze <arthurwtm@gmail.com>
2017-03-10 19:31:00 +0800

    Added doctor signup

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,4 +1,4 @@
-class PatientsController < ApplicationController
+class DoctorsController < ApplicationController
 	def new
 		@user = User.new
 	end
@@ -6,8 +6,8 @@ class PatientsController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		@user.save
-		@patient = Patient.new(user_id: @user.id)
-		@patient.save
+		@doctor = Doctor.new(user_id: @user.id, specialization: params[:specialization])
+		@doctor.save
 	end
 
 	private
@@ -15,4 +15,5 @@ class PatientsController < ApplicationController
 	def user_params
     params[:user].permit(:email, :password, :full_name, :identity_card, :street_address,:postcode, :city, :country, :contact_number)
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,6 @@ delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
 get "/sign_up" => "clearance/users#new", as: "sign_up"
 
 resources :patients, only: [:new, :create]
+resources :doctors, only: [:new, :create]
 
 end
