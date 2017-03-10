248347fc5cc064b51f74fd7c3267eae66a4dbf12
artze <arthurwtm@gmail.com>
2017-03-10 19:00:38 +0800

    Added patient registration

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,6 @@ get "/sign_in" => "clearance/sessions#new", as: "sign_in"
 delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
 get "/sign_up" => "clearance/users#new", as: "sign_up"
 
+resources :patients, only: [:new, :create]
+
 end
