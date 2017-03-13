283d007f009c691c151b986819e6041f667247e4
artze <arthurwtm@gmail.com>
2017-03-13 22:05:05 +0800

    renamed DOB column to date_of_birth in patients table

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,12 @@
 class Patient < ApplicationRecord
   belongs_to :user
   has_many :medical_records
+  validates_presence_of :DOB
+  validate :date_of_birth_in_past
+
+  def date_of_birth_in_past
+  	if DOB.present? && DOB > Date.today
+  		erros.add(:DOB, 'Date of birth can\'t be in future')
+  	end
+  end
 end
