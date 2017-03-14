d3364ce34d57861a47a3f76a45cdb55397d940a3
artze <arthurwtm@gmail.com>
2017-03-14 22:13:42 +0800

    Added validations on new attributes for patient model

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,7 +1,11 @@
 class Patient < ApplicationRecord
   belongs_to :user
   has_many :medical_records
-  validates_presence_of :date_of_birth
+  validates_presence_of :date_of_birth, :gender, :height, :weight, :blood_type
+  validates_inclusion_of :gender, in: %w( Male Female )
+  validates_inclusion_of :height, in: 20..250
+  validates_inclusion_of :weight, in: 5..400
+  validates_inclusion_of :blood_type, in: %w( O A B AB )
   validate :date_of_birth_in_past
 
   def date_of_birth_in_past
